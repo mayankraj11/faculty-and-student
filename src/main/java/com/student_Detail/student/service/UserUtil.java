1d43746cbfa49ababff47c5c6555a0cfba386480
Mayank Raj <mraj21760@gmail.com>
2025-08-19 21:43:47 +0530

    add changes

--- src/main/java/com/student_Detail/student/service/UserUtil.java
+++ src/main/java/com/student_Detail/student/service/UserUtil.java
@@ -4,7 +4,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserUtil {
+    /**
+     * This method reverses the input string and converts it to uppercase.
+     *
+     * @param str the input string to be reversed
+     * @return the reversed string in uppercase
+     */
     public String reverse(String str){
+        System.out.println("UserUtil class is created");
         return new StringBuilder(str).reverse().toString().toUpperCase();
     }
 }
